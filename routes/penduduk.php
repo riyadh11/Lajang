b5c343f9c65b1d127214a18b11343840db7ae386
ahmad riyadh al faathin <riyadh11@live.com>
2017-12-06 18:37:02 +0700

    Perubahan Routes Penduduk

--- routes/penduduk.php
+++ routes/penduduk.php
@@ -13,7 +13,7 @@ Route::post('/laporan/buat','Penduduk\LaporanController@store');
 Route::get('/laporan/ubah/{id}','Penduduk\LaporanController@gupdate');
 Route::post('/laporan/ubah/','Penduduk\LaporanController@update');
 
-Route::post('/laporan/perkembangan','Penduduk\KomentarController@store');
-Route::post('/laporan/perkembangan/ubah','Penduduk\KomentarController@update');
-Route::get('/laporan/perkembangan/hapus/{id}','Penduduk\KomentarController@remove');
+Route::post('/laporan/komentar','Penduduk\KomentarController@store');
+Route::post('/laporan/komentar/ubah','Penduduk\KomentarController@update');
+Route::get('/laporan/komentar/hapus/{id}','Penduduk\KomentarController@remove');
 Route::post('/laporan/vote','Penduduk\VoteController@store');
